a6b99dc1d0295385f8099ad003b6fd48018d3141
davidseol <hardroad07@gmail.com>
2022-09-23 13:47:49 +0900

    자바 과제 1번문제

---
+++ chap01/First.java
@@ -0,0 +1,13 @@
+//public class First {
+//    public static void main(String[] args) {
+//        int var1=5;
+//        int var2=2;
+//        double var3=var1/var2;
+//        int var4=(int)(var3*var2);
+//        System.out.println(var4);
+//    }
+//}
+
+//int는 정수만 담기때문에 5를2로 나누면 2.5가 2로만 인식되고
+//2x2가 되어서 4가 도출 되었다
+
